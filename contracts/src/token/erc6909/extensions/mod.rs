43bbbec47df5e8e69363718e48c939618e1ce048
iTranscend <llordbanks500@gmail.com>
2025-06-16 00:30:59 +0100

    feat: erc6909 metadata extension trait

--- contracts/src/token/erc6909/extensions/mod.rs
+++ contracts/src/token/erc6909/extensions/mod.rs
@@ -1,4 +1,7 @@
 //! Common extensions
+pub mod metadata;
+pub mod metadata;
 pub mod supply;
 
+pub use metadata::{Erc6909Metadata, IErc6909Metadata};
 pub use supply::{Erc6909Supply, IErc6909Supply};
