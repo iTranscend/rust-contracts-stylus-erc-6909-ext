6f44f71b8fdf8d02f4fa416d2d6111ad5a168b64
iTranscend <llordbanks500@gmail.com>
2025-06-16 01:32:45 +0100

    feat: erc6909 example implementation

--- examples/erc6909/src/lib.rs
+++ examples/erc6909/src/lib.rs
@@ -1,12 +1,20 @@
-use openzeppelin_stylus::token::erc6909::{
-    self, extensions::IErc6909Supply, Erc6909, IErc6909,
+#![cfg_attr(not(any(test, feature = "export-abi")), no_main)]
+#![allow(clippy::result_large_err)]
+extern crate alloc;
+
+use alloc::vec::Vec;
+
+use alloy_primitives::{Address, FixedBytes, U256};
+use openzeppelin_stylus::{
+    token::erc6909::{self, Erc6909, IErc6909},
+    utils::introspection::erc165::IErc165,
 };
 use stylus_sdk::prelude::*;
 
 #[entrypoint]
 #[storage]
 struct Erc6909Example {
-    ecr6909: Erc6909,
+    erc6909: Erc6909,
 }
 
 #[public]
@@ -17,7 +25,53 @@ impl Erc6909Example {}
 impl IErc6909 for Erc6909Example {
     type Error = erc6909::Error;
 
-    // TODO: implement core interface
+    fn transfer(
+        &mut self,
+        receiver: Address,
+        id: U256,
+        amount: U256,
+    ) -> Result<bool, Self::Error> {
+        self.erc6909.transfer(receiver, id, amount)
+    }
+
+    fn transfer_from(
+        &mut self,
+        sender: Address,
+        receiver: Address,
+        id: U256,
+        amount: U256,
+    ) -> Result<bool, Self::Error> {
+        self.erc6909.transfer_from(sender, receiver, id, amount)
+    }
+
+    fn approve(
+        &mut self,
+        spender: Address,
+        id: U256,
+        amount: U256,
+    ) -> Result<bool, Self::Error> {
+        self.erc6909.approve(spender, id, amount)
+    }
+
+    fn set_operator(
+        &mut self,
+        spender: Address,
+        approved: bool,
+    ) -> Result<bool, Self::Error> {
+        self.erc6909.set_operator(spender, approved)
+    }
+
+    fn balance_of(&self, owner: Address, id: U256) -> U256 {
+        self.erc6909.balance_of(owner, id)
+    }
+
+    fn allowance(&self, owner: Address, spender: Address, id: U256) -> U256 {
+        self.erc6909.allowance(owner, spender, id)
+    }
+
+    fn is_operator(&self, owner: Address, spender: Address) -> bool {
+        self.erc6909.is_operator(owner, spender)
+    }
 }
 
 #[public]
