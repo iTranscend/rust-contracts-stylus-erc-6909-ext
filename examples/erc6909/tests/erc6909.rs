dc848dddcb9596b4f1686a6087de0077b6e9659d
iTranscend <llordbanks500@gmail.com>
2025-06-18 10:06:33 +0100

    feat: erc6909 example integration test

--- examples/erc6909/tests/erc6909.rs
+++ examples/erc6909/tests/erc6909.rs
@@ -1,7 +1,28 @@
 #![cfg(feature = "e2e")]
 
 use abi::Erc6909;
+use alloy::primitives::U256;
+use e2e::Account;
 
 mod abi;
 
-// TODO: Define integration tests
+fn random_token_ids(size: usize) -> Vec<U256> {
+    (0..size).map(U256::from).collect()
+}
+
+// ============================================================================
+// Integration Tests: ERC-6909 Token
+// ============================================================================
+
+#[e2e::test]
+async fn balance_of_zero_balance(alice: Account) -> eyre::Result<()> {
+    let contract_addr = alice.as_deployer().deploy().await?.contract_address;
+    let contract = Erc6909::new(contract_addr, &alice.wallet);
+    let token_ids = random_token_ids(1);
+
+    let Erc6909::balanceOfReturn { balance } =
+        contract.balanceOf(alice.address(), token_ids[0]).call().await?;
+    assert_eq!(U256::ZERO, balance);
+
+    Ok(())
+}
