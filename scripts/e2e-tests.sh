b243c43027f1b333b7a37de766320ec550490270
iTranscend <llordbanks500@gmail.com>
2025-06-26 06:57:22 +0100

    fix(ci): update scripts to match main

--- scripts/e2e-tests.sh
+++ scripts/e2e-tests.sh
@@ -4,7 +4,9 @@ set -e
 # Navigate to project root
 cd "$(dirname "$(realpath "$0")")/.."
 
-cargo build --release --target wasm32-unknown-unknown -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort
+cargo build --release --target wasm32-unknown-unknown \
+  -Z build-std=std,panic_abort \
+  -Z build-std-features=panic_immediate_abort
 
 export RPC_URL=http://localhost:8547
 export DEPLOYER_ADDRESS=0xcEcba2F1DC234f70Dd89F2041029807F8D03A990
